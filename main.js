4dd8e9909a3055a0ff867345ecde4df73f76396d
ChrisLam <fdq09eca@gmail.com>
2022-09-20 16:16:09 +0100

    L8

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
+            onSales: true,
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariantIndex: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -17,8 +17,29 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        
+        updateSelectedVariantIndex(selectedVariantIndex) {
+            this.selectedVariantIndex = selectedVariantIndex
+            console.log(this.selectedVariantIndex)            
         }
+    },
+    computed: {
+        currentVariant() {
+            return this.variants[this.selectedVariantIndex]
+        },
+        image(){
+            return this.currentVariant.image
+        },
+        inStock(){
+            return this.currentVariant.quantity > 0
+        },
+        
+        title() {
+            if (this.onSales) {
+                return `${this.brand} ${this.product}`
+            }
+            return this.product
+        }
+        
     }
 })
